a49bd7f7e17e7ee90a1c805cffba107f923a0ccd
Diego Zahid Quispe Quispe <diego.quispe.qui@tecsup.edu.pe>
2025-05-06 15:09:44 -0500

    Creación e implementación de CartItem

---
+++ app/src/main/java/com/example/carrito/CartItem.kt
@@ -0,0 +1,11 @@
+package com.example.carrito
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "cart_items")
+data class CartItem(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val nombre: String,
+    val precio: Double
+)
